9b2b55ddae6cfbe3e75fef51aaab6578ea2ea7f5
Aaron Nowlan <aaron.nowlan@snowflake.com>
2025-06-06 13:19:00 -0700

    Update setup.sql
    adding help functions to generate fake test data

--- setup.sql
+++ setup.sql
@@ -21,9 +21,119 @@ CREATE OR REPLACE SCHEMA TASK_GRAPH_SCHEMA;
 GRANT OWNERSHIP ON DATABASE TASK_GRAPH_DATABASE TO ROLE TASK_GRAPH_ROLE COPY CURRENT GRANTS;
 GRANT OWNERSHIP ON ALL SCHEMAS IN DATABASE TASK_GRAPH_DATABASE TO ROLE TASK_GRAPH_ROLE COPY CURRENT GRANTS;
 
+
 --set event table at the database object
 ALTER DATABASE TASK_GRAPH_DATABASE SET EVENT_TABLE = TASK_GRAPH_DATABASE.TASK_GRAPH_SCHEMA.events_in_task_graph;
 ALTER ACCOUNT SET EVENT_TABLE = TASK_GRAPH_DATABASE.TASK_GRAPH_SCHEMA.events_in_task_graph;
+
+-- function to generate customer test data 
+create or replace function gen_cust_info(num_records number)
+returns table (custid number(10), cname varchar(100), spendlimit number(10,2))
+language python
+runtime_version=3.9
+handler='CustTab'
+packages = ('Faker')
+as $$
+from faker import Faker
+import random
+
+fake = Faker()
+# Generate a list of customers  
+
+class CustTab:
+    # Generate multiple customer records
+    def process(self, num_records):
+        customer_id = 1000 # Starting customer ID                 
+        for _ in range(num_records):
+            custid = customer_id + 1
+            cname = fake.name()
+            spendlimit = round(random.uniform(1000, 10000),2)
+            customer_id += 1
+            yield (custid,cname,spendlimit)
+
+$$;
+
+-- function to generate product inventory data 
+create or replace function gen_prod_inv(num_records number)
+returns table (pid number(10), pname varchar(100), stock number(10,2), stockdate date)
+language python
+runtime_version=3.9
+handler='ProdTab'
+packages = ('Faker')
+as $$
+from faker import Faker
+import random
+from datetime import datetime, timedelta
+fake = Faker()
+
+class ProdTab:
+    # Generate multiple product records
+    def process(self, num_records):
+        product_id = 100 # Starting customer ID                 
+        for _ in range(num_records):
+            pid = product_id + 1
+            pname = fake.catch_phrase()
+            stock = round(random.uniform(500, 1000),0)
+            # Get the current date
+            current_date = datetime.now()
+            
+            # Calculate the maximum date (3 months from now)
+            min_date = current_date - timedelta(days=90)
+            
+            # Generate a random date within the date range
+            stockdate = fake.date_between_dates(min_date,current_date)
+
+            product_id += 1
+            yield (pid,pname,stock,stockdate)
+
+$$;
+
+create or replace function gen_cust_purchase(num_records number,ndays number)
+returns table (custid number(10), purchase variant)
+language python
+runtime_version=3.9
+handler='genCustPurchase'
+packages = ('Faker')
+as $$
+from faker import Faker
+import random
+from datetime import datetime, timedelta
+
+fake = Faker()
+
+class genCustPurchase:
+    # Generate multiple customer purchase records
+    def process(self, num_records,ndays):       
+        for _ in range(num_records):
+            c_id = fake.random_int(min=1001, max=1999)
+            
+            #print(c_id)
+            customer_purchase = {
+                'custid': c_id,
+                'purchased': []
+            }
+            # Get the current date
+            current_date = datetime.now()
+            
+            # Calculate the maximum date (days from now)
+            min_date = current_date - timedelta(days=ndays)
+            
+            # Generate a random date within the date range
+            pdate = fake.date_between_dates(min_date,current_date)
+            
+            purchase = {
+                'prodid': fake.random_int(min=101, max=199),
+                'quantity': fake.random_int(min=1, max=5),
+                'purchase_amount': round(random.uniform(10, 1000),2),
+                'purchase_date': pdate
+            }
+            customer_purchase['purchased'].append(purchase)
+            
+            #customer_purchases.append(customer_purchase)
+            yield (c_id,purchase)
+
+$$;
+
 
 
 --OPTIONAL NOTIFIACTION TESTING--
